c841c1b1b0be9c27ec4fb211a4ccae4664aa5edc
Matias Linares <matiaslina@gmail.com>
2015-02-21 18:10:08 -0300

    Add origin model

--- hannibal/thumbs/admin.py
+++ hannibal/thumbs/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from thumbs.models import Channel
 
 # Register your models here.
+admin.site.register(Channel)
--- hannibal/thumbs/models.py
+++ hannibal/thumbs/models.py
@@ -1,5 +1,32 @@
 from django.db import models
+from django.conf import settings
+import os
 
 # Create your models here.
 class Channel(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, unique=True)
+
+    def save(self, *args, **kwargs):
+        """ Create a directory for the thumbnails. """
+        super(Channel, self).save(*args, **kwargs)
+        try:
+            os.mkdir(self.base_dir())
+        except OSError:
+            pass
+
+    def __str__(self):
+        return self.name
+
+    def base_dir(self):
+        return os.path.join(settings.THUMB_DIR, str(self.id))
+
+
+class Origin(models.Model):
+    channel = models.ForeignKey(Channel)
+    start_time = models.DateTimeField(null=False)
+    end_time = models.DateTimeField(null=False)
+    filename = models.CharField(max_length=200)
+
+    def duration(self):
+        """ Returns deltatime """
+        return self.end_time - self.start_time
