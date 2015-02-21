b65a7142f1804a8d1eaf2417a1bc70dca833efcf
Marcos Modenesi <marcosmodenesi@gmail.com>
2015-02-21 17:17:59 -0300

    added method __str__ to Channel

--- hannibal/thumbs/models.py
+++ hannibal/thumbs/models.py
@@ -3,3 +3,6 @@ from django.db import models
 # Create your models here.
 class Channel(models.Model):
     name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
