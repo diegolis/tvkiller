5a4ebbaef9ca08b365561f3e159329364612b9c4
Diego <diegolis@gmail.com>
2015-02-22 17:51:09 -0300

    Tap en circulito

--- yiya/www/js/controllers.js
+++ yiya/www/js/controllers.js
@@ -12,28 +12,37 @@ angular.module('starter.controllers', ['starter.services', 'ngCordova'])
     //$scope.thumbnails = Thumbnails.get($stateParams.channelId)
     $scope.thumbnails = []
     Thumbnails.get($stateParams.channelId, $scope);
-    $scope.channel = Channels.get($stateParams.channelId - 1)
+    $scope.channel = Channels.get($stateParams.channelId)
     $scope.position = 0
-
+    $scope.current_thumbnail = 0
 
     // share anywhere
-	$scope.share = function (thumbnail) {
+	$scope.share_image = function () {
+		thumbnail = $scope.thumbnails[$scope.current_thumbnail]
+		console.log($scope.channel);
 	    $cordovaSocialSharing.share('Look this image', $scope.channel.name, null, thumbnail.src);
 	}
 
     // share anywhere
-	$scope.share_video = function (thumbnail) {
+	$scope.share_video = function () {
+		//call to api
+		thumbnail = $scope.thumbnails[$scope.current_thumbnail]
 	    $cordovaSocialSharing.share('Look this video', $scope.channel.name, null, thumbnail.src);
 	}
-	
+
+	// cuando se mueve el slider	
 	$scope.go_to_position = function (position) {
-		$ionicScrollDelegate.$getByHandle("carousel").scrollTo(position/100*$scope.thumbnails.length*IMG_WIDTH, 0, true);
+		// convert position to current_thumbnail
+		$scope.current_thumbnail = position/100*$scope.thumbnails.length
+		$ionicScrollDelegate.$getByHandle("carousel").scrollTo($scope.current_thumbnail*IMG_WIDTH, 0, true);
 
 	}
 
+	// cuando se mueve el carousel
 	$scope.set_position = function () {
 		pos = $ionicScrollDelegate.$getByHandle("carousel").getScrollPosition().left;
-		$scope.position = pos / IMG_WIDTH;
-
+		$scope.current_thumbnail =  pos / IMG_WIDTH;
+		// convert current_thumbnail to position
+		$scope.position = $scope.current_thumbnail * 100 / $scope.thumbnails.length;
 	}
 })
