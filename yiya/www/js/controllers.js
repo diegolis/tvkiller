f324097d4f838a339712d9882b7341a77a0db7e5
Diego <diegolis@gmail.com>
2015-02-22 12:51:54 -0300

    Agregar slider

--- yiya/www/js/controllers.js
+++ yiya/www/js/controllers.js
@@ -9,12 +9,16 @@ angular.module('starter.controllers', ['starter.services', 'ngCordova'])
 .controller('ChannelCtrl', function($scope, $stateParams, Channels, Thumbnails, $cordovaSocialSharing) {
     $scope.thumbnails = Thumbnails.get($stateParams.channelId)
     $scope.channel = Channels.get($stateParams.channelId - 1)
+    $scope.position = 0
 
     // share anywhere
 	$scope.share = function (thumbnail) {
 	    $cordovaSocialSharing.share('Look this image', $scope.channel.name, null, thumbnail.src);
 	}
 	
+	$scope.go_to_thumbnail = function (thumbnail) {
+	    $cordovaSocialSharing.share('Look this image', $scope.channel.name, null, thumbnail.src);
+	}
 
 })
 
